ab0b5c640043b781a61278dd0639106c0703db36
Rob Blanckaert <basicer@basicer.com>
2024-09-05 21:21:47 -0700

    Patch it

--- patch.py
+++ patch.py
@@ -1,1 +1,17 @@
+import sys
+import urllib.request
+from subprocess import Popen, PIPE, STDOUT
+
+PRS_TO_ADD = [ 91263, 95788, 91884 ]
+
 print("Patching!")
+print("Python version: ", sys.version)
+
+for pr in PRS_TO_ADD:
+    url = "https://github.com/godotengine/godot/pull/" + str(pr) + ".patch"
+    print(url)
+    contents = urllib.request.urlopen(url).read()
+    sys.stdout.write(contents.decode('utf-8'))
+    p = Popen(['git', 'am'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    out = p.communicate(input=contents)[0]
+    print(out.decode('utf-8'))
