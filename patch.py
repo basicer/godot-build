ed3cdd20ee8b9dbf95ebe0363d670c587db6b414
Rob Blanckaert <basicer@basicer.com>
2024-10-19 22:44:13 -0700

    Add ability to push patche stuff

--- patch.py
+++ patch.py
@@ -1,14 +1,21 @@
 import sys
 import os
+import argparse
 import urllib.request
 from subprocess import check_output, Popen, PIPE, STDOUT
 
+parser = argparse.ArgumentParser(
+    prog='Patch.py',
+    description='Apply patches to Godot',
+)
+
+parser.add_argument('-p', '--push', action='store_true')
+
 PRS_TO_ADD = [
-    91263,
-    95788,
-    91884,
+    91884, # Specify key when loading pack
+    91263, # QOI Import
+    95788, # WASM Interperter
     96897, # Resource Preview request_draw_and_wait
-    96904, # Fix import deadlock
 ]
 
 print("Patching!")
@@ -24,6 +31,7 @@ if not ident or len(ident) < 5:
 	print(check_output("git config --global user.email patch@basicer.com".split(" ")))
 	print(check_output("git config --global user.name patch".split(" ")))
 
+args = parser.parse_args()
 
 my_env = os.environ.copy()
 
@@ -36,12 +44,15 @@ for pr in PRS_TO_ADD:
        pr = parts[1]
 
     url = "https://github.com/" + org + "/godot/pull/" + str(pr) + ".patch"
-    print(url)
+    print(f"Fetching {url} for patch.")
     contents = urllib.request.urlopen(url).read()
     p = Popen(['git', 'am'], stdout=PIPE, stdin=PIPE, stderr=STDOUT, env=my_env)
     out = p.communicate(input=contents)[0]
     print(out.decode('utf-8'))
     if p.returncode != 0:
        sys.exit(1)
+
+if args.push:
+    print(check_output("git push -f origin HEAD:patched".split(" ")))
 
 print("Jobs done")
